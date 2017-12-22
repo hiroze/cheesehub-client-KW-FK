e66dec7fc4eb7c0304122c7a42af082784b1ca7e
Kayla Webb <kaylarwebb@gmail.com>
2017-12-22 12:41:37 -0500

    Added async and sync actions

---
+++ client/src/actions/cheese.js
@@ -0,0 +1,27 @@
+const API_BASE_URL = '/api';
+
+export const FETCH_CHEEESES_REQUEST = 'FETCH_CHEESES_REQUEST'
+export const fetchCheesesRequest = () => ({
+  type: FETCH_CHEEESES_REQUEST
+});
+
+export const FETCH_CHEEESES_SUCCESS = 'FETCH_CHEESES_SUCCESS'
+export const fetchCheesesSuccess = () => ({
+  type: FETCH_CHEEESES_SUCCESS,
+  cheese
+});
+
+export const FETCH_CHEEESES_ERROR= 'FETCH_CHEESES_ERROR'
+export const fetchCheesesError = () => ({
+  type: FETCH_CHEEESES_ERROR,
+  error
+});
+
+export const fetchCheeses = () => {
+  return dispatch => {
+    fetch(`${API_BASE_URL}`/cheeses)
+    .then(res => res.json())
+    .then(data => dispatch(fetchCheesesSuccess(data)))
+    .catch(err => fetchCheeseError(err))
+  }
+}
