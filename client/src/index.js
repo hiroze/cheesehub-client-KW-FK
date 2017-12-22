224f4dda3f1d738ef84433ce681149b179119ee5
Firoz Kamdar <firozkamdar@gmail.com>
2017-12-22 11:47:28 -0800

    travis test

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import CheeseList from "./components/cheese-list";
 import registerServiceWorker from "./registerServiceWorker";
+import store from "./index-store";
 
 const data = ["Bath Blue", "Barkham Blue", "Buxton Blue"];
 
-ReactDOM.render(<CheeseList cheeses={data} />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <CheeseList cheeses={data} />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
-
-
