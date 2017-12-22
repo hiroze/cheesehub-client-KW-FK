5f5887136f2513c4519c43e9e6773d45e1425e27
Firoz Kamdar <firozkamdar@gmail.com>
2017-12-22 11:27:55 -0800

    netlify set up...hopefully

---
+++ client/src/config.js
@@ -0,0 +1,9 @@
+// 'use strict';
+
+// const DATABASE_URL = process.env.DATABASE_URL
+
+// exports.DATABASE_URL = DATABASE_URL;
+
+// exports.TEST_DATABASE_URL = 'mongodb://localhost';
+
+// exports.PORT = process.env.PORT || 8080;
